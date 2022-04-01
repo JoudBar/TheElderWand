9982d8e5676a6e56897e399de75628cf060ad893
jean-robertbotuna <78855325+jean-robertbotuna@users.noreply.github.com>
2022-04-01 19:23:45 +0200

    Ik heb de css van de inlog pagina en de account pagina veranderd omdat de knoppen niet in de center waren.

---
+++ assets/js/setsPopup.js
@@ -0,0 +1,40 @@
+
+
+document.getElementById("batmanbutton").addEventListener("click",()=>{
+    document.querySelector(".popup2").style.display = "flex";
+
+});
+document.getElementById("minecraftbutton").addEventListener("click",()=>{
+    document.querySelector(".popup2").style.display = "flex";
+
+});
+document.getElementById("mariobutton").addEventListener("click",()=>{
+    document.querySelector(".popup2").style.display = "flex";
+
+});
+document.getElementById("halobutton").addEventListener("click",()=>{
+    document.querySelector(".popup2").style.display = "flex";
+
+});document.getElementById("friendsbutton").addEventListener("click",()=>{
+    document.querySelector(".popup2").style.display = "flex";
+
+});
+document.getElementById("ninjagobutton").addEventListener("click",()=>{
+    document.querySelector(".popup2").style.display = "flex";
+
+});document.getElementById("policebutton").addEventListener("click",()=>{
+    document.querySelector(".popup2").style.display = "flex";
+
+});
+document.getElementById("trainbutton").addEventListener("click",()=>{
+    document.querySelector(".popup2").style.display = "flex";
+
+});
+
+
+
+
+document.getElementById("button4").addEventListener("click",()=>{
+    document.querySelector(".popup2").style.display = "none";
+
+});
