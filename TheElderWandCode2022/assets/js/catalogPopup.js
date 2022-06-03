a7588a579775a766feee7b8ded4b00b070237a82
jean-robertbotuna <78855325+jean-robertbotuna@users.noreply.github.com>
2022-06-03 20:00:52 +0200

    TheElderWandCode2022

---
+++ TheElderWandCode2022/assets/js/catalogPopup.js
@@ -0,0 +1,31 @@
+
+document.getElementById("legobutton").addEventListener("click",()=>{
+    document.querySelector(".popup3").style.display = "flex";
+
+});
+document.getElementById("fifabutton").addEventListener("click",()=>{
+    document.querySelector(".popup3").style.display = "flex";
+
+});
+
+document.getElementById("fortnitebutton").addEventListener("click",()=>{
+    document.querySelector(".popup3").style.display = "flex";
+
+});
+
+document.getElementById("magicbutton").addEventListener("click",()=>{
+    document.querySelector(".popup3").style.display = "flex";
+
+});
+
+document.getElementById("bankbutton").addEventListener("click",()=>{
+    document.querySelector(".popup3").style.display = "flex";
+
+});
+
+
+
+document.getElementById("button5").addEventListener("click",()=>{
+    document.querySelector(".popup3").style.display = "none";
+
+});
