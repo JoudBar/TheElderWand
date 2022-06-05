86ce636848de09056299089ae72cbc9a197d4804
SamihaAlamouri <samiha.alamouri@student.ap.be>
2022-06-05 17:37:15 +0200

    contact-form backend linken met frontend

--- TheElderWandCode2022/app.ts
+++ TheElderWandCode2022/app.ts
@@ -3,6 +3,8 @@ import ejs, { name } from 'ejs';
 import{MongoClient, ObjectId} from 'mongodb';
 const bcrypt = require('bcrypt');
 const session =  require('express-session');
+const bodyParser = require('body-parser');
+const nodemailer = require('nodemailer');
 
 const uri = "mongodb+srv://s133181:Papier10.@login.wqoqa.mongodb.net/?retryWrites=true&w=majority";
 
@@ -13,6 +15,8 @@ const app = express();
 app.set("port", 3000);
 app.set("view engine", "ejs");
 
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
 app.use(express.urlencoded({extended: true}));
 
 interface Person {
@@ -22,13 +26,89 @@ interface Person {
     password: string;
 }
 
+
 app.get("/", (req, res) => {
-    res.render('index');
+    res.render('TheOne');
 });
 app.post("/", (req, res) => {
-    res.render('index');
+    res.render('TheOne');
 });
 
+app.get("/faq", (req, res) => {
+    res.render('faq');
+});
+app.post("/faq", (req, res) => {
+    res.render('faq');
+});
+
+app.get("/personages", (req, res) => {
+    res.render('personages');
+});
+app.post("/personages", (req, res) => {
+    res.render('personages');
+});
+
+app.get("/catalogus", (req, res) => {
+    res.render('catalogus');
+});
+app.post("/catalogus", (req, res) => {
+    res.render('catalogus');
+});
+
+app.get("/contact", (req, res) => {
+    res.render('contact');
+});
+app.post("/contact", (req, res) => {
+    const output = `
+    <p>You have a new contact request</p>
+    <h3>Contact details</h3>
+    <ul>
+        <li>Name: ${req.body.name}</li>
+        <li>Email: ${req.body.email}</li>
+    </ul>
+    <h3>Message</h3>
+    <p>${req.body.message}</p>
+    `;
+
+    const transporter = nodemailer.createTransport({
+        service: 'outlook',
+        auth:{
+            user: 'theelderwandtheone@outlook.com',
+            pass: 'Theone1.'
+        },
+        tls:{
+            rejectUnauthorized:false
+          }
+    });
+    console.log(3);
+
+    const mailOptions = {
+        from: 'TheOne',
+        to: 'theelderwandtheone@outlook.com',
+        subject: 'Node contact request',
+        text : 'hello world',
+        html : output
+    };
+    console.log(4);
+
+    transporter.sendMail(mailOptions, (error: any, info: any) => {
+        console.log(5)
+        if (error) {
+            res.send('error')
+        console.log(6)
+        } else {
+            console.log('email sent: '+ info.response)
+            res.send('We hebben uw bericht goed ontvangen, we zullen zo snel mogelijk reageren op uw vragen!')
+        }
+    })
+});
+
+app.get("/quiz", (req, res) => {
+    res.render('quiz');
+});
+app.post("/quiz", (req, res) => {
+    res.render('quiz');
+});
 
 app.get("/register", (req, res) => {
     res.render("register");
